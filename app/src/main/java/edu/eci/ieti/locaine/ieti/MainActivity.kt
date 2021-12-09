ee18bb466bc188899be90482ed79887e32a8839d
IJuanchoG <ijuankhoxd@gmail.com>
2021-12-09 04:38:14 -0500

    Loading Card

--- app/src/main/java/edu/eci/ieti/locaine/ieti/MainActivity.kt
+++ app/src/main/java/edu/eci/ieti/locaine/ieti/MainActivity.kt
@@ -3,14 +3,26 @@ package edu.eci.ieti.locaine.ieti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.valentinilk.shimmer.shimmer
 import dagger.hilt.android.AndroidEntryPoint
+import edu.eci.ieti.locaine.ieti.model.User
 import edu.eci.ieti.locaine.ieti.ui.theme.IETITheme
 
 
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
             IETITheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MyApp()
+                    AddUser()
                 }
             }
         }
@@ -30,14 +42,113 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(viewModel: UserViewModel = hiltViewModel()) {
+fun AddUser(viewModel: UserViewModel = hiltViewModel()) {
+    val users by viewModel.users.observeAsState(initial = arrayListOf())
+    val isLoading by viewModel.isLoading.observeAsState(initial = false)
+    CreateInterface(
+        onAddClick = {
+            viewModel.addUser()
+        },
+        users = users,
+        isLoading = isLoading
 
+    )
 }
+
+@Composable
+fun CreateInterface(
+    onAddClick :( () -> Unit)? = null,
+    users: List<User>,
+    isLoading: Boolean
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Simple Rest + Room") },
+                actions = {
+                    IconButton(onClick = {
+                        onAddClick?.invoke()
+                    }) {
+                        Icon(Icons.Filled.Add, "Add")
+                    }
+                }
+            )
+        }
+    ) {
+        LazyColumn {
+            var itemCount = users.size
+            if( isLoading) itemCount++
+
+            items(count = itemCount){ index ->
+                var auxIndex = index
+                if(isLoading) {
+                    if (auxIndex == 0)
+                        return@items LoadingCard()
+                    auxIndex--
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun LoadingCard() {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 1.dp,
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()
+            .testTag("loadingCard")
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            ImageLoading()
+            Spacer()
+            Column {
+                Spacer()
+                Box(modifier = Modifier.shimmer()) {
+                    Column {
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                        Spacer()
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ImageLoading(){
+    Box(modifier = Modifier.shimmer()){
+        Box( modifier =
+            Modifier
+                    .size((50.dp))
+                    .background(Color.LightGray)
+        )
+    }
+}
+
+
+@Composable
+fun Spacer(size: Int = 8) = Spacer(modifier = Modifier.size(size.dp))
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IETITheme {
-        MyApp()
+        CreateInterface(onAddClick = null, users= listOf(), isLoading = false)
     }
 }
--- app/src/main/java/edu/eci/ieti/locaine/ieti/repository/UserRepository.kt
+++ app/src/main/java/edu/eci/ieti/locaine/ieti/repository/UserRepository.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import edu.eci.ieti.locaine.ieti.datasource.RestDataSource
 import edu.eci.ieti.locaine.ieti.model.User
 import edu.eci.ieti.locaine.ieti.model.UserDao
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 interface UserRepository {
@@ -18,6 +19,8 @@ class UserRepositoryImp @Inject constructor(
     private val userDao: UserDao
 ): UserRepository {
     override suspend fun getNewUser(): User {
+
+        delay(2000)
         val name = dataSource.getUserName().results[0].name!!
         val location = dataSource.getUserLocation().results[0].location!!
         val picture = dataSource.getUserPicture().results[0].picture!!
