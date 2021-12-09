b82c7adcc4f7c65f750f1a1ac7145be52587f082
IJuanchoG <ijuankhoxd@gmail.com>
2021-12-09 04:53:35 -0500

    APP FUNCIONAL!!

--- app/src/main/java/edu/eci/ieti/locaine/ieti/MainActivity.kt
+++ app/src/main/java/edu/eci/ieti/locaine/ieti/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.eci.ieti.locaine.ieti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,16 +11,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
+import coil.compose.rememberImagePainter
 import com.valentinilk.shimmer.shimmer
 import dagger.hilt.android.AndroidEntryPoint
 import edu.eci.ieti.locaine.ieti.model.User
@@ -49,6 +53,9 @@ fun AddUser(viewModel: UserViewModel = hiltViewModel()) {
         onAddClick = {
             viewModel.addUser()
         },
+        onDeleteClick = {
+            viewModel.deleteUser(it)
+        },
         users = users,
         isLoading = isLoading
 
@@ -57,14 +64,15 @@ fun AddUser(viewModel: UserViewModel = hiltViewModel()) {
 
 @Composable
 fun CreateInterface(
-    onAddClick :( () -> Unit)? = null,
+    onAddClick :( () -> Unit) ? = null,
+    onDeleteClick: ((toDelete: User) -> Unit) ? = null,
     users: List<User>,
     isLoading: Boolean
 ){
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Simple Rest + Room") },
+                title = { Text("IETI TODO") },
                 actions = {
                     IconButton(onClick = {
                         onAddClick?.invoke()
@@ -86,6 +94,43 @@ fun CreateInterface(
                         return@items LoadingCard()
                     auxIndex--
                 }
+                val user = users[auxIndex]
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = 1.dp,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .fillMaxWidth(),
+                ){
+                    Row(modifier = Modifier.padding(8.dp)) {
+                        Image(
+                            modifier = Modifier.size(50.dp),
+                            painter = rememberImagePainter(
+                                data = user.thumbnail,
+                                builder = {
+                                    placeholder(R.drawable.ic_launcher_background)
+                                    error(R.drawable.ic_launcher_background)
+                                }
+                            ),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillHeight
+                        )
+                        Spacer()
+                        Column(
+                            Modifier.weight(1f),
+                        ) {
+                            Text("${user.name} ${user.lastName}")
+                            Text(user.city)
+                        }
+                        Spacer()
+                        IconButton(onClick = {
+                            onDeleteClick?.invoke(user)
+                        }) {
+                            Icon(Icons.Filled.Delete, "Remove")
+                        }
+                    }
+                }
+
 
             }
         }
@@ -149,6 +194,6 @@ fun Spacer(size: Int = 8) = Spacer(modifier = Modifier.size(size.dp))
 @Composable
 fun DefaultPreview() {
     IETITheme {
-        CreateInterface(onAddClick = null, users= listOf(), isLoading = false)
+        CreateInterface(users= listOf(), isLoading = false)
     }
 }
